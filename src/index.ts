c627e676d5a8af46d5d5ca33e432ac56a971eceb
shivam <thelegendofawizard@gamil.com>
2020-06-20 01:05:42 +0530

    change 3

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,12 @@ import { postRouter } from './routers/post';
 import { likeRouter } from './routers/like';
 import { commentRouter } from './routers/comment';
 import { followRouter } from './routers/follow';
-import cors from 'cors';
 import path from 'path';
 
 require('./db/mongoose'); // no need to save in variable(only to ensure that database runs)
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 const port = process.env.PORT;
 
 app.use(express.json());
